dc00b643a055c47781eefbb6d20a1953d0203935
EddyDeltaV <Eduardo.santos25@compscihigh.org>
2025-02-04 15:28:18 +0000

    i got mostly everything just missing my for loop

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// https://chatgpt.com/share/679cec68-fb24-8006-bbb7-df611e1af80e
 const express = require('express')
 const app = express()
 
@@ -6,7 +7,7 @@ const inventory = [
     category: "Exotic Fruits",
     items: [
       {
-        name: "Apples",
+        name: "Rose Apples",
         cost: 127.00,
         imageUrls: ["https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRN0nJl9UCN9tR3ACRFPcayj-Q8gGE4lSxwVwNI6EcA9f2sfs97W4ipELkf6bGdOPpSi9LDRWxTxU2gOe0_XdSzdcfG-bMjOOKruOG8Fj-r"],
         path: "/item/0",
@@ -32,66 +33,70 @@ const inventory = [
     category: "Dishes",
     items: [
       {
-        name: "Summer Dress",
+        name: "Sushi with cavier ",
         cost: 29.99,
-        imageUrl: "https://ullajohnson.com/cdn/shop/files/ULLA_JOHNSON_BlaireDress__BLANC_01_MAIN.jpg?v=1714067305&width=960",
+        imageUrl: "https://media.post.rvohealth.io/wp-content/uploads/sites/3/2020/02/325225_2200-732x549.jpg",
         path: "/item/3",
         quantity: 100,
-        colors: ["pink", "yellow", "white"]
       },
       {
-        name: "Leather Handbag",
+        name: "Fugu",
         cost: 99.99,
-        imageUrl: "https://www.letanneur.us/cdn/shop/files/TEMI1014G05-d_c4c045a7-8a31-4b47-943a-85913ad5c3df.jpg?v=1736851853",
+        imageUrl: "https://d1o84luq0jqpf6.cloudfront.net/rails/active_storage/blobs/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBbGlwIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--4387c345eb6bbe0a6f379db316ce02a023849be5/26318224_m.jpg",
         path: "/item/4",
         quantity: 40,
-        colors: ["brown", "black"]
       },
       {
-        name: "Running Shoes",
+        name: "Grilled Dog",
         cost: 89.99,
-        imageUrl: "https://fullscopesports.com/wp-content/uploads/2022/07/Topo-Athletic-Women-Ultrafly-4-Comfortable-Lightweight-5MM-Drop-Road-Running-Shoes-Profile.jpg",
+        imageUrl: "https://www.rover.com/blog/wp-content/uploads/iStock-610774614-e1652301832537.jpg",
         path: "/item/5",
         quantity: 60,
-        colors: ["blue", "pink", "white"]
-      }
+      }]
+  },
+  {
+    category: "Desserts",
+    items: [
       {
-        category: "Desserts",
-        items: [
-          {
-            name: "Summer Dress",
-            cost: 29.99,
-            imageUrl: "https://ullajohnson.com/cdn/shop/files/ULLA_JOHNSON_BlaireDress__BLANC_01_MAIN.jpg?v=1714067305&width=960",
-            path: "/item/3",
-            quantity: 100,
-            colors: ["pink", "yellow", "white"]
-          },
-          {
-            name: "Leather Handbag",
-            cost: 99.99,
-            imageUrl: "https://www.letanneur.us/cdn/shop/files/TEMI1014G05-d_c4c045a7-8a31-4b47-943a-85913ad5c3df.jpg?v=1736851853",
-            path: "/item/4",
-            quantity: 40,
-            colors: ["brown", "black"]
-          },
-          {
-            name: "Running Shoes",
-            cost: 89.99,
-            imageUrl: "https://fullscopesports.com/wp-content/uploads/2022/07/Topo-Athletic-Women-Ultrafly-4-Comfortable-Lightweight-5MM-Drop-Road-Running-Shoes-Profile.jpg",
-            path: "/item/5",
-            quantity: 60,
-            colors: ["blue", "pink", "white"]
-          }
-    ]
+        name: "Creme Puff",
+        cost: 29.99,
+        imageUrl: "https://sugarspunrun.com/wp-content/uploads/2023/01/Cream-Puffs-Recipe-1-of-1.jpg",
+        path: "/item/3",
+        quantity: 100,
+      },
+      {
+        name: "Candy Apple",
+        cost: 99.99,
+        imageUrl: "https://sugargeekshow.com/wp-content/uploads/2021/09/candy_apple_recipe_fb.jpg",
+        path: "/item/4",
+        quantity: 40,
+      },
+      {
+        name: "Cloud Cake",
+        cost: 89.99,
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTauYLrjNmHiiGTWUq-SVw_pXgT1ffhZpJTGQ&s",
+        path: "/item/5",
+        quantity: 60,
+      }
+        
+]
   }
 ]
 
 
+app.set("view engine", "ejs")
 
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path)
+  next()
+})
 
+app.use(express.static(__dirname + "/public"))
 
-
+app.get("/mens", (req, res)=>{
+  res.render("category.ejs", inventory[0])
+})
 
 app.listen(3000, () => {
-    console.log("Server running")
-  })
+  console.log("Server running")
+})
